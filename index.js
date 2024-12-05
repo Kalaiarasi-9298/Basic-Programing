5f2af7e8239d701918063a25de555d324d931c5e
Kalaiarasi-9298 <kalaiarasi9298@gmail.com>
2024-12-05 12:56:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function primeOrNot(num){
+    if(num<=1){
+        return`${num} is not a prime number`;
+    }
+    for(let i=2; i<num; i++){
+        if(num%i==0){
+            return`${num} is not a prime number`
+        }
+    }
+    
+        return`${num} is a prime number`;
+    }
+   console.log(primeOrNot(15))
+   console.log(primeOrNot(11))
